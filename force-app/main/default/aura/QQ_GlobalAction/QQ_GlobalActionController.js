d4c5f16234d87710f68600534637b009d2b38ae0
Alex McDonald <amcdonald@salesforce.com>
2024-01-19 18:46:26 +1100

    Added flexibility to deal with namespace or no namespace

--- force-app/main/default/aura/QQ_GlobalAction/QQ_GlobalActionController.js
+++ force-app/main/default/aura/QQ_GlobalAction/QQ_GlobalActionController.js
@@ -1,10 +1,14 @@
 ({
 	init : function(component, event, helper) {
+        
+        const namespace = component.getConcreteComponent().getDef().getDescriptor().getNamespace();
+        const pageApiName = (namespace=='c') ? 'QQ_New_Quote' : namespace+'__QQ_New_Quote';
+
         var navService = component.find('navService');
         var pageReference = {
             type: 'standard__navItemPage',
             attributes: {
-                apiName: 'quickerquotes__QQ_New_Quote'
+                apiName: pageApiName
             }
         }
         navService.navigate(pageReference);
